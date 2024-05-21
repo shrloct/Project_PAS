7ceac9cbf2ee9f7df107a1026805d099fa3150e5
SahrulR <sahrulromadhonsmktibazma@gmail.com>
2024-05-21 09:06:42 +0700

    pembaruan dengan tambahan define table category, event

---
+++ db/tables/event.js
@@ -0,0 +1,46 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config");
+
+const event = sequelize.define('event', {
+    id: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false
+    },
+    id_guru: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes,
+        allowNull: false
+    },
+    category: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    location: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    max_participant: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    status: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+});
